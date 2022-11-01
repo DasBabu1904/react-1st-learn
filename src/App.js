81caaa705d77e4435c55e4e813ad53c4955cc536
Das_Babu <dass7550@gmail.com>
2022-11-01 11:25:47 +0600

    simple edit how to use html(xml)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+const testNum = 1093;
+const obj = { name: "Sourav", work: "programming" }
+const styleObj = {
+  color: "purple",
+  backgroundColor: "yellow",
+  borderRadius: "10px"
+}
 function App() {
   return (
     <div className="App">
@@ -8,6 +15,15 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>JSX</h1>
+          <div className="container">
+            <h3>Working with react</h3>
+            <p>the number is: {testNum}</p>
+            <h6>Details</h6>
+            <p style={styleObj}>work:{obj.work}<br />
+              Name:{obj.name}
+            </p>
+          </div>
         </p>
         <a
           className="App-link"
